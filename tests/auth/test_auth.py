8a4abdbca140ce8bd422926260deec42efeea41a
Alfa_Q <Joshrmatsushima@hotmail.com>
2019-02-19 15:22:16 -0700

    Improve auth Package Test Coverage

--- tests/auth/test_auth.py
+++ tests/auth/test_auth.py
@@ -4,22 +4,48 @@ tests.auth.test_auth
 Testing the Pixiv authentication API.
 """
 
+import pytest
 from pixivpy import auth
+from pixivpy.common import exceptions
 from tests.config import Config
 
 
 def test_get_auth_token_sanity():
     """
-    Ensure that the json response is valid, assuming credentials are valid.
+    Ensure that the tuple response is not NULL.
     """
-    json = auth.get_auth_token(Config.TEST_EMAIL, Config.TEST_PASSWORD)
-    assert json is not None
+    result_tuple = auth.get_auth_token(Config.TEST_EMAIL, Config.TEST_PASSWORD)
+    assert result_tuple is not None
 
 
-def test_get_auth_token_json():
+def test_get_auth_token_tuple():
     """
-    Ensure that the json response is type Dict
+    Ensure that the parsed response is type tuple.
     """
-    json = auth.get_auth_token(Config.TEST_EMAIL, Config.TEST_PASSWORD)
-    assert dict == type(json)
+    result_tuple = auth.get_auth_token(Config.TEST_EMAIL, Config.TEST_PASSWORD)
+    assert tuple == type(result_tuple)
 
+def test_get_auth_token_subtypes():
+    """
+    Ensure that the authentication token response is a tuple of str, int
+    """
+    tkn, ttl = auth.get_auth_token(Config.TEST_EMAIL, Config.TEST_PASSWORD)
+    assert str == type(tkn)
+    assert int == type(ttl)
+
+def test_get_auth_token_values():
+    """
+    Ensure that the value of the auth bearer token is non-empty and the value of the time-to-live
+    is greater than 0.
+    """
+    tkn, ttl = auth.get_auth_token(Config.TEST_EMAIL, Config.TEST_PASSWORD)
+    assert 0 < len(tkn)
+    assert 0 < ttl
+
+def test_get_auth_token_fails_bad_status():
+    """
+    Ensure that the auth.get_auth_token function fails due to an InvalidStatusCode exception
+    for bad credentials.
+    """
+    with pytest.raises(exceptions.InvalidStatusCode):
+        auth.get_auth_token('', '')
--- tests/auth/test_models.py
+++ tests/auth/test_models.py
@@ -4,6 +4,8 @@ tests.auth.test_models
 Testing the Pixiv authentication API model.
 """
 
+import pytest
+from pixivpy.common import exceptions
 from pixivpy.auth import models
 from tests.config import Config
 
@@ -22,3 +24,12 @@ def test_model_get_auth_token_json():
     """
     json = models.get_auth_token(Config.TEST_EMAIL, Config.TEST_PASSWORD)
     assert dict == type(json)
+
+
+def test_model_get_auth_token_fails_bad_status():
+    """
+    Ensure that the models.get_auth_token function fails due to an InvalidStatusCode exception
+    for bad credentials.
+    """
+    with pytest.raises(exceptions.InvalidStatusCode):
+        models.get_auth_token('', '')

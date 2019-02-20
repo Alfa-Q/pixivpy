1435204a69e68280b2efc8ef68694ae660c9f4a4
Alfa_Q <Joshrmatsushima@hotmail.com>
2019-02-19 21:37:23 -0700

    Add Placeholder Test to Fix Build Errors
    Temporary workaround for Travis CI exit code 5 error.  Will be removed after main API functions are implemented.

---
+++ tests/test_placeholder.py
@@ -0,0 +1,11 @@
+"""
+tests.test_placeholder
+----------------------
+A placeholder test file containing a single testcase that does nothing.
+This helps work around the exit code 5 error in Travis CI.
+Will be removed once the main API functions are implemented.
+"""
+
+
+def test_placeholder():
+    pass
